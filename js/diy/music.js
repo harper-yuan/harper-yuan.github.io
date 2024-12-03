6b414f1450ce15eb4a2abfcda38f5ad3e7788275
harper-yuan <2454670070@qq.com>
2024-12-04 02:10:55 +0800

    Site updated: 2024-12-04 02:10:54

--- js/diy/music.js
+++ js/diy/music.js
@@ -6,8 +6,8 @@ const ap = new APlayer({
 	{
         name: "where we're going",
         artist: 'elijah woods',
-        url: '//music.163.com/outchain/player?type=2&id=2064620976&auto=1&height=66',
-        cover: 'https://p1.music.126.net/byjfkEIOWI_RmxSKEWTPyw==/18910500486297525.jpg?param=200y200',	
+        url: 'https://m804.music.126.net/20241204022425/3e25279427f6af7e1f8e9f686e38da8f/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/29405971135/b0ee/ccc4/f397/b102c6b41dc18bfa72eeb1f6793adfde.mp3?authSecret=000001938dad4df311c40a3b1db04865',
+        cover: '//p1.music.126.net/i29ug6vPubo0OyOovEs2Aw==/109951168744612304.jpg?param=90y90',	
     },
 	]
 });
