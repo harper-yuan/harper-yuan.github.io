069a15add169206e113c2c59fcdeffabd450328d
harper-yuan <2454670070@qq.com>
2024-12-04 01:53:29 +0800

    Site updated: 2024-12-04 01:53:28

--- dist/music.js
+++ dist/music.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>const ap = new APlayer({
+const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
 	autoplay: true, //自动播放
